3b8aa18d794586a1433a7db13ab1ad2b7ecd94ed
Daniel Hyres <danhyres@gmail.com>
2022-09-22 18:58:04 -0400

    comment modal add

--- controllers/posts.js
+++ controllers/posts.js
@@ -16,8 +16,6 @@ module.exports = {
     try {
       const posts = await Post.find().sort({ createdAt: "desc" }); //Post is the model (required above), .lean() (mongoose) is for getting the raw object from mongo (documents on mongo, while similar to "objects" actually include more than you need) this will be faster
       console.log(`These are your posts ${posts}`)
-      
-
       res.render("feed2.ejs", { posts: posts });
     } catch (err) {
       console.log(err);
