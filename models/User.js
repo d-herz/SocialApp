c1e4c71f81aa920d49c06255ae027f3c3e2499de
Daniel Hyres <danhyres@gmail.com>
2022-12-13 12:52:41 -0500

    Added member since field to User model and profile view

--- models/User.js
+++ models/User.js
@@ -2,9 +2,18 @@ const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  userName: { type: String, unique: true },
-  email: { type: String, unique: true },
+  userName: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String, unique: true
+  },
   password: String,
+  signUpDate: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 // Password hash middleware.
